9231f8b378e107cb1f26b26198de2c95809aa9b4
Tom Paterson <tompaterson1996@gmail.com>
2023-08-18 10:44:15 +0100

    initial header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from "./components/Header";
+
+const App = () => {
   return (
     <div className="App">
+      <Header title="Images Gallery"/>
       <h1>Image Gallery</h1>
     </div>
   );
