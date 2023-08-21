6162758ba8da5dc1e4c6d20cdd5cad372ced198b
Tom Paterson <tompaterson1996@gmail.com>
2023-08-21 15:25:30 +0100

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
   const [word, setWord] = useState("")
 
   const handleSearchSubmit = (ev) => {
     ev.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   return (
