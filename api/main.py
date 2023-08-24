511891c88bf21922aa99ec4168e1dc8298e2b014
Tom Paterson <tompaterson1996@gmail.com>
2023-08-24 15:49:47 +0100

    added request to unsplash api and added linter

--- api/main.py
+++ api/main.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv(dotenv_path="./api/.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please provide UNSPLASH_KEY in .env.local file")
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    response = requests.get(
+        UNSPLASH_URL,
+        headers={"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"},
+        params={"query": word},
+    )
+
+    data = response.json()
+    return data
+
 
 if __name__ == "__main__":
-  app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello world!"
+    app.run()
