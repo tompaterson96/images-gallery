601a4095833be7b85cd93d92269f9add8c8f0860
Tom Paterson <tompaterson1996@gmail.com>
2023-08-29 13:36:35 +0100

    pymongo and mongo test

---
+++ api/mongo_client.py
@@ -0,0 +1,24 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./api/.env.local")
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
+
+client = MongoClient(
+    host=MONGO_URL,
+    port=MONGO_PORT,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+)
+
+
+def insert_test_document():
+    db = client.test
+    test_collection = db.test_collection
+    res = test_collection.insert_one({"name": "Tom", "company": "bjss"})
+    print(res)
